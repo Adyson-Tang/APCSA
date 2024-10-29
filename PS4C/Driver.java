77fce4be542e38226c859f1b1d43cf4201f87dc0
Adyson-Tang <adysontang@gmail.com>
2024-10-29 13:17:01 -0700

    Add files via upload

---
+++ PS4C/Driver.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class Driver here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Driver { 
+    public static void main (String[] args) {
+        //dont have to create new
+       // double n = sentimentVal("hi");
+       //String word = sentimentVal(0.5);
+         System.out.println(Review.totalSentiment("SimpleReview.txt"));
+         System.out.println(Review.starRating("SimpleReview.txt"));
+}
+    // instance variables - replace the example below with your own
+}
