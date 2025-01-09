3791ad6a145ba40bc493dde361f05f3b9d13e2a7
Adyson-Tang <adysontang@gmail.com>
2025-01-09 14:57:05 -0800

    Add files via upload

---
+++ Problem Set 5A/Driver.java
@@ -0,0 +1,59 @@
+
+public class Driver {
+    public static void main (String[] args) {
+        
+        Fraction test1 = new Fraction ();
+        Fraction test2 = new Fraction (1, 2);
+        Fraction test3 = new Fraction ("1/2");
+        Fraction test4 = new Fraction (test1);
+
+        System.out.println("Constructor Testing:");
+        
+        System.out.println (test1);
+        System.out.println (test2);
+        System.out.println (test3);
+        System.out.println (test4);
+
+        System.out.println(" ");
+
+        //testing accessor methods
+        Fraction am = new Fraction (2, 6);
+        
+        System.out.println("Accessor Methods Testing:");
+        
+        
+        System.out.println (am.getNum());
+        System.out.println (am.getDenom());
+        System.out.println (am.toString());
+        System.out.println (am.toDouble());
+
+        System.out.println(" ");
+        
+        //Fraction mm = new Fraction (2, 2);
+        
+        Fraction half = new Fraction(2, 5);
+        Fraction third = new Fraction (1, 10);
+        
+        System.out.println("Static Methods Testing: Multiply: ");
+        Fraction product = Fraction.multiply(half, third);
+        System.out.println(product);
+        
+        System.out.println("");
+        
+        System.out.println("Static Methods Testing: Divide: ");      
+        Fraction quotient = Fraction.divide(half, third);
+        System.out.println(quotient);
+        
+        System.out.println("");
+        
+        System.out.println("Static Methods Testing: Add: "); 
+        Fraction sum = Fraction.add(half, third);
+        System.out.println(sum);
+        
+        System.out.println("");
+        
+        System.out.println("Static Methods Testing: Subtract: ");
+        Fraction diff = Fraction.subtract(half, third);
+        System.out.println(diff);
+    }
+}
