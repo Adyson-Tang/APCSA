cf87b0e3f28b5a89dfc1684e9d3b8b7b1e2568a5
Adyson-Tang <adysontang@gmail.com>
2025-01-26 20:13:48 -0800

    Add files via upload

---
+++ Problem Set 1C/Classify.java
@@ -0,0 +1,27 @@
+public class Classify {
+    public static void main (String [] args){
+        /*
+        int z;
+        int x = 1;
+        x = x + 3;
+        int i = 9 * 20;
+        */
+        int x = 6;
+        int y;
+        x++;
+        System.out.println(x);
+        int a = 8*6-14%3+8/2;
+        System.out.println (a);
+        double t = 4;
+         t *= 2 + 1;
+         System.out.println(t);
+        
+        double h = 3 /(double) 4;
+         System.out.println(h);
+        int num;
+        
+        String str = "Darth Vader";
+        System.out.println (str.indexOf("art"));
+        System.out.println (str.indexOf(" "));
+    }
+}
